4bcdb99925b12b435fa9c5726afcaeaaaeb38101
saman <saman.tohidian@techvertu.co.uk>
2024-02-29 10:42:30 +0000

    file change

--- wp-content/uploads/uag-plugin/assets/5000/uag-js-5280-1709133003.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(){ window.addEventListener( 'load', function(){
-	UAGBTableOfContents._run( {"mappingHeaders":[true,true,true,true,true,true],"scrollToTop":false,"makeCollapsible":false}, '.uagb-block-21596d4e' );
-} );
- });
