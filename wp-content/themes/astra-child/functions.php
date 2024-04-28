7813b8513657ac11a491c966c1d14d6cbcf00667
saman <saman.tohidian@techvertu.co.uk>
2024-04-28 15:29:59 +0100

    change button alignment on licencing page and change the console alerts

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -20,7 +20,7 @@ function child_enqueue_styles() {
 
 	wp_enqueue_style( 'astra-child-theme-css', get_stylesheet_directory_uri() . '/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
 	wp_enqueue_style( 'astra-child-theme-custom-css', get_stylesheet_directory_uri() . '/assets/css/style.css', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
-	wp_enqueue_style( 'astra-child-theme-web-font','http://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
+	wp_enqueue_style( 'astra-child-theme-web-font','https://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
 	wp_enqueue_script('astra-child-scrollbar-scripts', get_template_directory_uri() . '-child/assets/js/jquery.scrollbar.min.js', array(), CHILD_THEME_ASTRA_CHILD_VERSION, true);
 	wp_enqueue_script('astra-child-scripts', get_template_directory_uri() . '-child/assets/js/scripts.js', array(), CHILD_THEME_ASTRA_CHILD_VERSION, true);
 
