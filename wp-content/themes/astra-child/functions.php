61238956828084347b1308738a686287e641b194
saman <saman.tohidian@techvertu.co.uk>
2024-06-19 09:32:31 +0100

    deqeue style

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -23,11 +23,15 @@ function child_enqueue_styles() {
 	wp_enqueue_style( 'astra-child-theme-web-font','https://fonts.googleapis.com/css?family=Lato&subset=latin,latin-ext', array('astra-theme-css'), CHILD_THEME_ASTRA_CHILD_VERSION, 'all' );
 	wp_enqueue_script('astra-child-scrollbar-scripts', get_template_directory_uri() . '-child/assets/js/jquery.scrollbar.min.js', array(), CHILD_THEME_ASTRA_CHILD_VERSION, true);
 	wp_enqueue_script('astra-child-scripts', get_template_directory_uri() . '-child/assets/js/scripts.js', array(), CHILD_THEME_ASTRA_CHILD_VERSION, true);
+	
 
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
 
+add_action('wp_footer', function () {
+	wp_dequeue_style('core-block-supports-inline-css');
+});
 get_template_part('includes/astrachild-config', 'config');
 
 get_template_part('includes/socialSharer', 'config');
